d2dc194b7884127929b040d66216b84ca130c899
donginLee <donginyee@gmail.com>
2024-05-27 15:38:19 +0900

    feat:Location 구현

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,1 +1,49 @@
-// location.jsx
+import React from "react";
+import { MdOutlineContentCopy } from "react-icons/md";
+
+export default function Location() {
+  const copyToClipboard = async (text) => {
+    await navigator.clipboard.writeText(text);
+  };
+
+  const address = "서울시 강남구 논현로 740";
+  const phone = "02-2278-9977";
+
+  return (
+    <div className="flex flex-col items-center p-8 bg-white pt-10 pt-16 pb-16">
+      <p className="text-heading2 text-theme1-black  text-darkGray font-continuous mt-5">
+        Location
+      </p>
+      <img
+        src="/src/assets/images/map.png"
+        alt="오시는 길 지도"
+        className="w-full h-auto mb-6 mt-9"
+      />
+
+      <div className="text-center font-nanum text-subheading mb-2">
+        아펠가모 광화문 B2 로스타노홀
+        <br />
+        <div className="mt-3">
+          <span className="flex justify-center text-body1 items-center">
+            {address}
+            <MdOutlineContentCopy
+              className="ml-2 cursor-pointer"
+              style={{ color: "#848484" }}
+              onClick={() => copyToClipboard(phone)}
+            />
+          </span>
+        </div>
+        <div className="text-center font-nanum text-body1">
+          <span className="flex justify-center itext-body items-center">
+            {phone}
+            <MdOutlineContentCopy
+              className="ml-2 cursor-pointer"
+              style={{ color: "#848484" }}
+              onClick={() => copyToClipboard(phone)}
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
