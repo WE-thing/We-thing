6edf5ffe70b9a0aec2a1945f70473b16685461c8
donginLee <donginyee@gmail.com>
2024-05-27 15:39:02 +0900

    feat: Gallery 구현

---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Photos from "../../lib/modules/Photos";
+
+export default function Gallery() {
+  return (
+    <div className="text-center bg-theme1-primary w-full p-5 pb-12">
+      <p className="font-continuous text-heading2 text-darkGray my-10">
+        Gallery
+      </p>
+      <Photos
+        link={"/src/assets/images/weddingphoto"}
+        format={"jpeg"}
+        start={1}
+        end={9}
+      />
+    </div>
+  );
+}
