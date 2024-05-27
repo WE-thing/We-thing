2bbabfb88688252771683d119579d056ad311d61
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-28 08:47:43 +0900

    feat: 요구사항 정리

--- src/components/sharedAlbum/SharedAlbum.jsx
+++ src/components/sharedAlbum/SharedAlbum.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function SharedAlbum() {
+// 사진들을 불러와서 배열에 띄울 거에요
+// GET /album/:id 뭐 이런거 하겠죠?
+// 불러온 사진 목록을 state에 넣으면 될 것 같아요
+
+// 각 사진을 누르면 onClick으로 크게 모달에 띄울거에요
+
+// '+' 버튼이 있어요 -> 사진을 업로드 할거에요
+// 기기의 앨범을 띄워서 사진을 고르게 할거에요
+// 고른 사진도 state에 저장하면 되겠네요
+// POST /album/:userId 하면 되겠죠?
+
+// 다운로드 버튼이 있어요 -> 사진을 다운로드 할 거에요 우와
+// 어떻게 하는지는 아직은 모르겠어요 앞으로 배워갑시다
+
+
   return <div>SharedAlbum</div>;
 }
