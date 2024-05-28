294a33cc9ac1c328d460ab994685189311763359
KimRiun <kimriun27@gmail.com>
2024-05-28 16:48:22 +0900

    fix: proxy 이용해서 cors 해결

--- src/lib/apis/album.js
+++ src/lib/apis/album.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function getAlbumList({userId}) {
-    const resp = await axios.get(`api/album/${userId}`);
-    return resp.data;
+export async function getAlbumList({ userId }) {
+  const resp = await axios.get(`/api/album/${userId}`);
+  return resp.data;
 }
 
-export async function postAlbum({userId}) {
-    const resp = await axios.post(`/api/album/${userId}`);
-    return resp.data;
+export async function postAlbum({ userId }) {
+  const resp = await axios.post(`/api/album/${userId}`);
+  return resp.data;
 }
