a01a54d4d94c73c536a0002d6b5a66d38aae9050
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-31 09:51:55 +0900

    feat: 안쓰는 함수 제거

--- src/lib/apis/info.js
+++ src/lib/apis/info.js
@@ -8,11 +8,3 @@ export async function getInfo({token}) {
     });
     return resp.data;
 }
-
-export async function updateInfo({token}) {
-    const resp = await axios.post('/api/info', {
-        headers: {
-            'authorization': token,
-        }
-    })
-}
